33b79fd17b816470a33b4fa4c01e15bc616ea355
DOOMED2WIN <timon-steinhauser@web.de>
2024-07-16 15:21:00 +0200

    bugfix(contest-form): added missing imports

--- src/app/features/contest/admin/form/contest-admin-form.module.ts
+++ src/app/features/contest/admin/form/contest-admin-form.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,6 +10,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { DatetimeFormComponent } from 'ngx-cinlib/date/datetime-form';
+import { RichtextEditorFormComponent } from 'ngx-cinlib/forms/richtext';
 import { FormStepComponent, FormStepperComponent } from 'ngx-cinlib/forms/stepper';
 import { I18nDirective, TranslatablePipe } from 'ngx-cinlib/i18n';
 import { GridColumnDirective, GridRowComponent } from 'ngx-cinlib/layouts/grid-layout';
@@ -16,6 +18,7 @@ import { PageTitleComponent } from 'ngx-cinlib/layouts/title';
 import { MediaFormComponent } from 'ngx-cinlib/media/forms';
 import { CoreModule } from 'src/app/core/core.module';
 import { ContestFilterModule } from 'src/app/shared/filter/contest/contest-filter.module';
+import { ContactFormComponent } from 'src/app/shared/form/contact/contact-form.component';
 import { ContestAdminFormComponent } from './component/contest-admin-form.component';
 import { contestAdminFormStateKey } from './constants/contest-admin-form.constants';
 import { ContestAdminFormRoutingModule } from './contest-admin-form-routing.module';
@@ -28,11 +31,11 @@ const components = [
 
 const framework = [
   CommonModule,
-  FormsModule,
   ReactiveFormsModule,
 ];
 
 const materials = [
+  MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
   MatOptionModule,
@@ -50,6 +53,7 @@ const libs = [
   StoreModule.forFeature(contestAdminFormStateKey, contestAdminFormReducer),
   EffectsModule.forFeature([ContestAdminFormEffects]),
 
+  ContactFormComponent,
   DatetimeFormComponent,
   FormStepComponent,
   FormStepperComponent,
@@ -58,6 +62,7 @@ const libs = [
   I18nDirective,
   MediaFormComponent,
   PageTitleComponent,
+  RichtextEditorFormComponent,
   TranslatablePipe,
 ];
 
