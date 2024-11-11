857750b1ee454fedd0f1545129728e5bf1925c55
vetemi <valmir@codeschluss.de>
2024-11-11 18:07:59 +0100

    fix(ios): fixed testing

--- src/app/core/services/device.service.ts
+++ src/app/core/services/device.service.ts
@@ -5,18 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class DeviceService {
 
-  // public isIOs: boolean = [
-  //   'iPad Simulator',
-  //   'iPhone Simulator',
-  //   'iPod Simulator',
-  //   'iPad',
-  //   'iPhone',
-  //   'iPod'
-  // ].includes(navigator.platform)
-  // // iPad on iOS 13 detection
-  // || (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+  public isIOs: boolean = [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ].includes(navigator.platform)
+  // iPad on iOS 13 detection
+  || (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
 
-  public isIOs = true;
+  // public isIOs = true;
 
   constructor() {
     document.documentElement.style.setProperty('--header-height', '7rem');
